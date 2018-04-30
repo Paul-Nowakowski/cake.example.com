2a55c9a893ef6da0b3ed4b2b6912eaa07b5c1542
paul <tuner777@yahoo.com>
2018-04-30 16:12:21 -0500

    added CakeDC's user plugin :

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -215,3 +215,10 @@ Type::build('timestamp')
 if (Configure::read('debug')) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
 }
+Plugin::load(
+    'CakeDC/Users',
+    [
+        'routes'=>true,
+        'bootstrap'=>true
+    ]
+);
